c0454600cd84917e03e644cd36737781d11e237b
Vishwa K <vishwa0604@gmail.com>
2025-08-17 18:33:44 +0530

    Fixed App.js: navbar conditional rendering and location handling

--- src/WindowSize.js
+++ src/WindowSize.js
@@ -3,9 +3,9 @@ import {useEffect, useState} from 'react'
 const useWindowSize=()=> {
   
     const [windowSize, setWindowSize] = useState({
-        width: undefined,
-        height: undefined
-    });
+  width: window.innerWidth,
+  height: window.innerHeight
+});
     useEffect(()=>{
         const handleResize = () => {
             setWindowSize({

802b51b0a17dafc15f28c666711d50f73b253830
Vishwa K <vishwa0604@gmail.com>
2025-08-17 18:28:17 +0530

    Fixed App.js: navbar conditional rendering and location handling

--- src/App.js
+++ src/App.js
@@ -1,74 +1,81 @@
-import {Routes, Route, Link, useLocation} from 'react-router-dom'
+import { Routes, Route, Link, useLocation } from 'react-router-dom'
 import './App.css'
 import Home from './Home'
-
 import Certificates from './Certificates'
 import Projects from './Projects'
 import Missing from './Missing'
 import useWindowSize from './WindowSize'
 import { GiHamburgerMenu } from "react-icons/gi";
 import './Mobile.css';
+import { useState } from 'react';
 
 function App() {
-  const location = useLocation()
-  const {width} = useWindowSize();
-   if(location.pathname == '/' && width <700){
-      document.getElementById('navBar').style.width = '70%';
-     }
-     else{
-      document.getElementById('navBar').style.width = '100%';
+  const location = useLocation();
+  const { width } = useWindowSize();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-     }
-  const moreOptions = ()=>{
-     const nav=document.querySelector('#slideNav');
-     nav.style.right = '150px'
-    
-    }
-    const close=()=>{
-     document.querySelector('#slideNav').style.right = '-999px';
+  const moreOptions = () => setIsMenuOpen(true);
+  const close = () => setIsMenuOpen(false);
 
-
-    }
-  
   return (
     <div>
-      
-        <section id='navBar'>
-          <h2>Portfolio</h2>
-          <div>
-            {width > 600 ?
-            
+      <section 
+        id='navBar'
+        style={{
+          width: (location.pathname === '/' && width < 700) ? '70%' : '100%'
+        }}
+      >
+        <h2>Portfolio</h2>
+        <div>
+          {width > 600 ? (
             <nav>
               <ul>
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/certificates'}>Certificates</Link></li>
                 <li><Link to={'/projects'}>Projects</Link></li>
-                <li><a href='https://drive.google.com/file/d/1OCQ34b2wP8XnW1yVtEf6hutlRxP56JLQ/view?usp=drivesdk'>Resume</a></li>
-
+                <li>
+                  <a href='https://drive.google.com/file/d/1OCQ34b2wP8XnW1yVtEf6hutlRxP56JLQ/view?usp=drivesdk'>
+                    Resume
+                  </a>
+                </li>
               </ul>
             </nav>
-            : <button className='more' onClick={()=> moreOptions()}><GiHamburgerMenu /></button>}
-            <nav id='slideNav'>
-              <ul>
-                <li onClick={()=>close()}>X</li>
-                <li><Link to={'/'}>Home</Link></li>
-                <li><Link to={'/certificates'}>Certificates</Link></li>
-                <li><Link to={'/projects'}>Projects</Link></li>
-                <li><a href='https://drive.google.com/file/d/1OCQ34b2wP8XnW1yVtEf6hutlRxP56JLQ/view?usp=drivesdk'>Resume</a></li>
+          ) : (
+            <button className='more' onClick={moreOptions}>
+              <GiHamburgerMenu />
+            </button>
+          )}
 
-              </ul>
-            </nav>
-          </div>
-        </section>
-        
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/certificates' element={<Certificates />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='*' element={<Missing />} />
-        </Routes>
+          <nav 
+            id='slideNav' 
+            style={{ right: isMenuOpen ? "150px" : "-999px" }}
+          >
+            <ul>
+              <li onClick={close}>X</li>
+              <li><Link to={'/'} onClick={close}>Home</Link></li>
+              <li><Link to={'/certificates'} onClick={close}>Certificates</Link></li>
+              <li><Link to={'/projects'} onClick={close}>Projects</Link></li>
+              <li>
+                <a 
+                  href='https://drive.google.com/file/d/1OCQ34b2wP8XnW1yVtEf6hutlRxP56JLQ/view?usp=drivesdk'
+                  onClick={close}
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/certificates' element={<Certificates />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='*' element={<Missing />} />
+      </Routes>
     </div>
-  )
-
+  );
 }
-export default App
+
+export default App;
